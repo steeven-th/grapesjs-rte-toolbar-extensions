121fe213afaa66d3b9c52c295e47e566f53e3ada
Steeven <82022828+steeven-th@users.noreply.github.com>
2025-06-30 21:37:21 +0200

    feat: first commit

--- README.md
+++ README.md
@@ -1,1 +1,214 @@
-# grapesjs-rte-toolbar-extensions
+# Grapesjs RTE Toolbar Extensions
+
+[DEMO](https://codepen.io/steeven-th/pen/jEPRYPJ)
+
+### HTML
+```html
+<link href="https://unpkg.com/grapesjs/dist/css/grapes.min.css" rel="stylesheet">
+<script src="https://unpkg.com/grapesjs"></script>
+<script src="https://unpkg.com/grapesjs-rte-toolbar-extensions"></script>
+
+<div id="gjs"></div>
+```
+
+### JS
+```js
+const editor = grapesjs.init({
+	container: '#gjs',
+  height: '100%',
+  fromElement: true,
+  storageManager: false,
+  plugins: ['grapesjs-rte-toolbar-extensions'],
+});
+```
+
+### CSS
+```css
+body, html {
+  margin: 0;
+  height: 100%;
+}
+```
+
+
+## Summary
+
+* Plugin name: `grapesjs-rte-toolbar-extensions`
+* Description: This plugin extends the default GrapesJS Rich Text Editor (RTE) toolbar by adding a wide range of customizable text formatting tools. It includes buttons for inline styles (bold, italic, underline, strikethrough), font families and sizes, headings, alignment, quotes, lists, indentation, subscript/superscript, case transformation, horizontal line, undo/redo, and even a custom “span” wrapper with its own GrapesJS toolbar support.
+  Each feature is modular and can be enabled or disabled individually through options.
+
+
+## Options
+
+## Options
+
+| Option                  | Description                                                                                   | Default     |
+|-------------------------|-----------------------------------------------------------------------------------------------|-------------|
+| `base.bold`             | Enables the **bold** button.                                                                  | `true`      |
+| `base.italic`           | Enables the **italic** button.                                                                | `true`      |
+| `base.underline`        | Enables the **underline** button.                                                             | `true`      |
+| `base.strikethrough`    | Enables the **strikethrough** button.                                                         | `true`      |
+| `base.link`             | Enables the **link** button.                                                                  | `true`      |
+| `fonts.span`            | Adds a **wrap in span** button.                                                               | `true`      |
+| `fonts.fontName`        | Array of font names to be displayed in a dropdown. Set to `false` to disable.                 | `false`     |
+| `fonts.fontSize`        | Enables a **font size** dropdown selector.                                                    | `false`     |
+| `format.heading1`       | Adds **H1** format button.                                                                     | `true`      |
+| `format.heading2`       | Adds **H2** format button.                                                                     | `true`      |
+| `format.heading3`       | Adds **H3** format button.                                                                     | `true`      |
+| `format.heading4`       | Adds **H4** format button.                                                                     | `true`      |
+| `format.heading5`       | Adds **H5** format button.                                                                     | `true`      |
+| `format.heading6`       | Adds **H6** format button.                                                                     | `true`      |
+| `format.paragraph`      | Adds **Paragraph** format button.                                                              | `true`      |
+| `format.quote`          | Adds a **blockquote** format button.                                                          | `true`      |
+| `subscriptSuperscript`  | Adds **subscript** and **superscript** buttons.                                               | `true`      |
+| `indentOutdent`         | Adds **indent** and **outdent** buttons.                                                      | `true`      |
+| `list`                  | Adds **ordered list** and **unordered list** buttons.                                         | `true`      |
+| `align`                 | Adds **text alignment** buttons: left, center, right, justify.                                | `true`      |
+| `actions`               | Reserved for future use (currently unused).                                                   | `true`      |
+| `undoredo`              | Adds **undo** and **redo** buttons.                                                           | `true`      |
+| `extra`                 | Adds **uppercase**, **lowercase**, and **horizontal line** tools.                             | `true`      |
+| `icons`                 | Object allowing to override default button icons. Example: `{ heading1: '<b>H1</b>' }`.       | `{}`        |
+| `maxWidth`              | Optional CSS `max-width` for the toolbar element. Example: `"700px"`                          | `undefined` |
+
+
+## Download
+
+* CDN
+    * `https://unpkg.com/grapesjs-rte-toolbar-extensions`
+* NPM
+    * `npm i grapesjs-rte-toolbar-extensions`
+* GIT
+    * `git clone https://github.com/steeven-th/grapesjs-rte-toolbar-extensions.git`
+
+
+
+## Usage
+
+Directly in the browser
+```html
+<link href="https://unpkg.com/grapesjs/dist/css/grapes.min.css" rel="stylesheet"/>
+<script src="https://unpkg.com/grapesjs"></script>
+<script src="path/to/grapesjs-rte-toolbar-extensions.min.js"></script>
+
+<div id="gjs"></div>
+
+<script type="text/javascript">
+  var editor = grapesjs.init({
+      container: '#gjs',
+      // ...
+      plugins: ['grapesjs-rte-toolbar-extensions'],
+      pluginsOpts: {
+        'grapesjs-rte-toolbar-extensions': { /* options */ }
+      }
+  });
+</script>
+```
+
+Modern javascript
+```js
+import grapesjs from 'grapesjs';
+import rteToolbarExtensionsPlugin from 'grapesjs-rte-toolbar-extensions';
+import 'grapesjs/dist/css/grapes.min.css';
+
+const editor = grapesjs.init({
+  container : '#gjs',
+  // ...
+  plugins: [rteToolbarExtensionsPlugin],
+  pluginsOpts: {
+    [rteToolbarExtensionsPlugin]: { /* options */ }
+  }
+  // or
+  plugins: [
+    editor => rteToolbarExtensionsPlugin(editor, { /* options */ }),
+  ],
+});
+```
+
+### Example
+
+```js
+import grapesjs from 'grapesjs';
+import rteToolbarExtensionsPlugin from 'grapesjs-rte-toolbar-extensions';
+import 'grapesjs/dist/css/grapes.min.css';
+
+const editor = grapesjs.init({
+    container : '#gjs',
+    height: '100%',
+    fromElement: true,
+    storageManager: false,
+    plugins: [rteToolbarExtensionsPlugin],
+    pluginsOpts: {
+        [rteToolbarExtensionsPlugin]: {
+            base: {
+                bold: true,
+                italic: true,
+                underline: true,
+                strikethrough: false,
+                link: true,
+            },
+            fonts: {
+                span: true,
+                fontName: ['Arial', 'Georgia', 'Courier New'],
+                fontSize: true,
+            },
+            format: {
+                heading1: true,
+                heading2: true,
+                heading3: false,
+                heading4: false,
+                heading5: false,
+                heading6: false,
+                paragraph: true,
+                quote: true,
+            },
+            subscriptSuperscript: true,
+            indentOutdent: true,
+            list: true,
+            align: true,
+            actions: true,
+            undoredo: true,
+            extra: true,
+            icons: {
+                heading1: '<b>H1</b>',
+                heading2: '<b>H2</b>',
+                uppercase: '<span style="font-weight:bold;">UP</span>',
+            },
+            maxWidth: '700px',
+        },
+    }
+});
+```
+
+
+## Development
+
+Clone the repository
+
+```sh
+$ git clone https://github.com/steeven-th/grapesjs-rte-toolbar-extensions.git
+$ cd grapesjs-rte-toolbar-extensions
+```
+
+Install dependencies
+
+```sh
+$ npm i
+```
+
+Start the dev server
+
+```sh
+$ npm start
+```
+
+Build the source
+
+```sh
+$ npm run build
+```
+
+
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,226 @@
+import en from './locale/en';
+import de from './locale/de';
+import fr from './locale/fr';
+
+export default (editor, opts = {}) => {
+    const options = {
+        ...{
+            base: {
+                bold: true,
+                italic: true,
+                underline: true,
+                strikethrough: true,
+                link: true,
+            },
+            fonts: {
+                span: true,
+            },
+            format: {
+                heading1: true,
+                heading2: true,
+                heading3: true,
+                heading4: true,
+                heading5: true,
+                heading6: true,
+                paragraph: true,
+                quote: true,
+            },
+            subscriptSuperscript: true,
+            indentOutdent: true,
+            list: true,
+            align: true,
+            actions: true,
+            undoredo: true,
+            extra: true,
+            icons: {},
+        },
+        ...opts,
+    };
+
+    const { icons } = options;
+    const formatBlock = "formatBlock";
+    const rte = editor.RichTextEditor;
+    const t = (key) => editor.I18n?.t(`grapesjs-rte-toolbar-extensions.${key}`) || key;
+
+    const fontNames = options.fonts.fontName
+        ? Array.isArray(options.fonts.fontName)
+            ? options.fonts.fontName
+            : false
+        : false;
+
+    options.fonts.fontName = fontNames;
+
+    const fontOptionsEl = fontNames
+        ? fontNames
+            .map((font) => "<option>" + font.toString() + "</option>")
+            .join("")
+        : "";
+
+    const fontNamesEl = `<select style="height:1.8rem;color:inherit;" class="gjs-field gjs-field-select">
+          ${fontOptionsEl}
+      </select>`;
+
+    editor.onReady(() => {
+        if (options.maxWidth)
+            rte.getToolbarEl().firstChild.style.maxWidth = options.maxWidth;
+
+        if (!options.base || typeof options.base === "object") {
+            !options.base.bold && rte.remove("bold");
+            !options.base.italic && rte.remove("italic");
+            !options.base.underline && rte.remove("underline");
+            !options.base.strikethrough && rte.remove("strikethrough");
+            !options.base.link && rte.remove("link");
+        }
+
+        const addButton = (name, icon, title, exec, extra = {}) => {
+            rte.add(name, {
+                icon,
+                attributes: {
+                    title: t(title),
+                    ...extra,
+                },
+                result: (rte) => rte.exec(exec),
+            });
+        };
+
+        options.base.bold && addButton("bold", icons.bold, "bold", "bold");
+        options.base.italic && addButton("italic", icons.italic, "italic", "italic");
+        options.base.underline && addButton("underline", icons.underline, "underline", "underline");
+        options.base.strikethrough && addButton("strikethrough", icons.strikethrough, "strikethrough", "strikethrough");
+        options.base.link && addButton("link", icons.link, "link", "createLink");
+
+        options.fonts?.fontName && rte.add("fontName", {
+            icon: fontNamesEl,
+            event: "change",
+            attributes: {
+                style: "padding: 0 4px 2px;",
+                title: t("fontName"),
+            },
+            result: (rte, action) =>
+                rte.exec("fontName", action.btn.firstChild.value),
+            update: (rte, action) => {
+                const value = rte.doc.queryCommandValue(action.name);
+                if (value != "false") {
+                    action.btn.firstChild.value = value;
+                }
+            },
+        });
+
+        options.fonts?.fontSize && rte.add("fontSize", {
+            icon: `<select style="height:1.8rem;color:inherit;" class="gjs-field gjs-field-select">
+                <option value="1">xx-small</option>
+                <option value="2">x-small</option>
+                <option value="3">small</option>
+                <option value="4">medium</option>
+                <option value="5">large</option>
+                <option value="6">x-large</option>
+                <option value="7">xx-large</option>
+              </select>`,
+            event: "change",
+            attributes: {
+                style: "padding: 0 4px 2px;",
+                title: t("fontSize"),
+            },
+            result: (rte, action) =>
+                rte.exec("fontSize", action.btn.firstChild.value),
+            update: (rte, action) => {
+                const value = rte.doc.queryCommandValue(action.name);
+                if (value != "false") {
+                    action.btn.firstChild.value = value;
+                }
+            },
+        });
+
+        const addFormatButton = (name, label) => {
+            options.format?.[name] && addButton(name, icons[name] || `<div>${label.toUpperCase()}</div>`, name, formatBlock, { tag: name });
+        };
+
+        ["heading1","heading2","heading3","heading4","heading5","heading6","paragraph","quote"].forEach(type => {
+            const label = type === "paragraph" ? "¶" : type.replace("heading", "H");
+            addFormatButton(type, label);
+        });
+
+        options.indentOutdent && addButton("indent", icons.indent, "indent", "indent");
+        options.indentOutdent && addButton("outdent", icons.outdent, "outdent", "outdent");
+        options.subscriptSuperscript && addButton("subscript", icons.subscript || "<div>X<sub>2</sub></div>", "subscript", "subscript");
+        options.subscriptSuperscript && addButton("superscript", icons.superscript || "<div>X<sup>2</sup></div>", "superscript", "superscript");
+
+        options.extra && rte.add("uppercase", {
+            icon: icons.uppercase || "<div>ABC</div>",
+            attributes: { title: t("uppercase") },
+            result: (rte) => {
+                const sel = rte.doc.getSelection();
+                if (!sel.rangeCount) return;
+                const range = sel.getRangeAt(0);
+                const span = rte.doc.createElement("span");
+                span.style.textTransform = "uppercase";
+                span.textContent = range.toString();
+                range.deleteContents();
+                range.insertNode(span);
+            },
+        });
+
+        options.extra && rte.add("lowercase", {
+            icon: icons.lowercase || "<div>abc</div>",
+            attributes: { title: t("lowercase") },
+            result: (rte) => {
+                const sel = rte.doc.getSelection();
+                if (!sel.rangeCount) return;
+                const range = sel.getRangeAt(0);
+                const span = rte.doc.createElement("span");
+                span.style.textTransform = "lowercase";
+                span.textContent = range.toString();
+                range.deleteContents();
+                range.insertNode(span);
+            },
+        });
+
+        options.list && addButton("olist", icons.olist, "olist", "insertOrderedList");
+        options.list && addButton("ulist", icons.ulist, "ulist", "insertUnorderedList");
+        options.align && addButton("justifyLeft", icons.justifyLeft, "justifyLeft", "justifyLeft");
+        options.align && addButton("justifyCenter", icons.justifyCenter, "justifyCenter", "justifyCenter");
+        options.align && addButton("justifyRight", icons.justifyRight, "justifyRight", "justifyRight");
+        options.align && addButton("justifyFull", icons.justifyFull, "justifyFull", "justifyFull");
+        options.extra && addButton("line", icons.line || "<b>&#8213;</b>", "line", "insertHorizontalRule");
+        options.undoredo && addButton("undo", icons.undo, "undo", "undo");
+        options.undoredo && addButton("redo", icons.redo, "redo", "redo");
+    });
+
+    editor.Commands.add('unwrap-span', {
+        run(editor) {
+            const selected = editor.getSelected();
+            if (!selected) return;
+            const el = selected.getEl();
+            if (el.tagName !== 'SPAN') return;
+            const parent = el.parentNode;
+            while (el.firstChild) parent.insertBefore(el.firstChild, el);
+            parent.removeChild(el);
+            editor.selectRemove(selected);
+        },
+    });
+
+    editor.DomComponents.addType('inline-span', {
+        isComponent: el => el.tagName === 'SPAN' && el.classList.contains('gjs-inline-span-in-paragraph'),
+        model: {
+            defaults: {
+                tagName: 'span',
+                draggable: true,
+                droppable: false,
+                editable: true,
+                stylable: true,
+                toolbar: [
+                    { command: 'move', label: '<i class="fa fa-arrows-alt"></i>' },
+                    { command: 'tlb-clone', label: '<i class="fa fa-clone"></i>' },
+                    { command: 'unwrap-span', label: '<i class="fa fa-ban"></i>', attributes: { title: t('unwrapSpan') } },
+                    { command: 'tlb-delete', label: '<i class="fa fa-trash"></i>' },
+                ],
+            },
+        },
+    });
+
+    editor.I18n && editor.I18n.addMessages({
+        en,
+        fr,
+        de,
+    });
+};
